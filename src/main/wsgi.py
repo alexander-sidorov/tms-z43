9ec1d45cd98d70103d9f34547f47d942cf3cc8c0
Alexander Sidorov <alexander@sidorov.dev>
2021-01-11 20:14:00 +0300

    implemented printing wsgi environ on main page

--- src/main/wsgi.py
+++ src/main/wsgi.py
@@ -1,3 +1,5 @@
+import random
+
 import sentry_sdk
 
 from framework.util.settings import get_setting
@@ -15,21 +17,32 @@ def application(environ, start_response):
         "Content-type": "text/html",
     }
 
+    random_number = random.randint(-100, 100)
+
+    environ2 = ""
+
+    for key, value in environ.items():
+        text = f"<tr><td>{key}</td><td>{value}</td></tr>"
+        environ2 = environ2 + text
+
     payload = (
-        b"<!DOCTYPE html>"
-        b"<html>"
-        b"<head>"
-        b"<title>Alpha</title>"
-        b'<meta charset="utf-8">'
-        b"</head>"
-        b"<body>"
-        b"<h1>Project Alpha</h1>"
-        b"<hr>"
-        b"<p>This is a template project.</p>"
-        b"</body>"
-        b"</html>"
+        "<!DOCTYPE html>"
+        "<html>"
+        "<head>"
+        "<title>Alpha</title>"
+        '<meta charset="utf-8">'
+        "</head>"
+        "<body>"
+        f"<h1>Project Alpha: {random_number}</h1>"
+        "<hr>"
+        "<p>Environ</p>"
+        "<table>"
+        f"{environ2}"
+        "</table>"
+        "</body>"
+        "</html>"
     )
 
     start_response(status, list(headers.items()))
 
-    yield payload
+    yield from [payload.encode()]
