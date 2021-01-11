605dd9c4b56c3e0d08314da674f130e1b4e91f5c
Alexander Sidorov <alexander@sidorov.dev>
2021-01-11 21:52:48 +0300

    implemented templates

--- src/main/wsgi.py
+++ src/main/wsgi.py
@@ -2,6 +2,7 @@ import random
 
 import sentry_sdk
 
+from framework.dirs import DIR_SRC
 from framework.util.settings import get_setting
 
 sentry_sdk.init(get_setting("SENTRY_DSN"), traces_sample_rate=1.0)
@@ -26,6 +27,7 @@ def application(environ, start_response):
         environ2 = environ2 + text
 
     template = read_template("index.html")
+
     payload = template.format(
         random_number=random_number,
         environ=environ2,
@@ -34,3 +36,15 @@ def application(environ, start_response):
     start_response(status, list(headers.items()))
 
     yield payload.encode()
+
+
+def read_template(template_name: str) -> str:
+    dir_templates = DIR_SRC / "main" / "templates"
+    template = dir_templates / template_name
+
+    assert template.is_file()
+
+    with template.open("r") as fd:
+        content = fd.read()
+
+    return content
