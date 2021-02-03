fc2f21ff673f53f757c221d8130d4d7d86dba0a1
Alexander Sidorov <76160017+alexander-sidorov@users.noreply.github.com>
2021-02-03 19:45:06 +0300

    Lesson 07: tests (#6)
    * added tests for task 303
    
    * added more tests for task 303
    
    * version bump
    
    * version bump
    
    * version bump
    
    Co-authored-by: Alexander Sidorov <alexander@sidorov.dev>

--- src/tasks/lesson03/task303.py
+++ src/tasks/lesson03/task303.py
@@ -5,7 +5,6 @@ def ask_user_to_input_a_sentence() -> str:
 
 def extract_words_from_sentence(s: str) -> list:
     w = s.split(" ")
-    assert len(w) == 2, f"error! sentence '{s}' contains <>2 words"
     return w
 
 
@@ -15,7 +14,18 @@ def render_template(t: str, c: dict) -> str:
 
 
 def solution(sentence: str) -> str:
+    """
+    Splits a sentence to two words, exchanges them
+    and adds "!" to each side of new sentence.
+    """
+
     words = extract_words_from_sentence(sentence)
+    if len(words) < 2:
+        word = words[0]
+        return f"!{word}!"
+
+    if len(words) > 2:
+        raise ValueError("function does not support sentences with > 2 words")
 
     template = "!{word2} {word1}!"
     context = {
