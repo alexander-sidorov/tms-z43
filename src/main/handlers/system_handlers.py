2f81c4a35d6410dc64dc383bd2f77326acd3bb82
Alexander Sidorov <alexander@sidorov.dev>
2021-02-03 20:39:08 +0300

    added missing files

---
+++ src/main/handlers/system_handlers.py
@@ -0,0 +1,17 @@
+import traceback
+
+from main.wsgi import ResponseT
+
+
+def handle_404(method: str, path: str, qs: str) -> ResponseT:
+    status = "404 Not Found"
+    content_type = "text/plain"
+    payload = f"OOPS! endpoint {path} not found!"
+    return status, content_type, payload
+
+
+def handle_500(method: str, path: str, qs: str) -> ResponseT:
+    status = "500 Internal Server Error"
+    content_type = "text/plain"
+    payload = traceback.format_exc()
+    return status, content_type, payload
