7b2eeebf8ffc31ba99c4bf04ddcac03e0f188142
Alexander Sidorov <76160017+alexander-sidorov@users.noreply.github.com>
2021-02-07 06:36:13 +0300

    Lesson 08~10 (#7)
    * added handler for 500
    
    * added missing files
    
    * added custom types
    
    * refactored logic
    
    * added task 1.01
    
    * added task 3.01, 3.02, 3.09
    
    * format
    
    * html complex
    
    Co-authored-by: Alexander Sidorov <alexander@sidorov.dev>

--- src/main/wsgi.py
+++ src/main/wsgi.py
@@ -1,104 +1,29 @@
-from typing import Callable
-from typing import Tuple
-from urllib.parse import parse_qs
-
 import sentry_sdk
 
-from framework.dirs import DIR_SRC
 from framework.util.settings import get_setting
-from tasks.lesson03 import task303
+from main.handlers import get_handler
+from main.handlers import handle_500
+from main.util import build_request
 
 sentry_sdk.init(get_setting("SENTRY_DSN"), traces_sample_rate=1.0)
 
-ResponseT = Tuple[str, str, str]
-
-
-def handle_error(method: str, path: str, qs: str) -> ResponseT:
-    payload = str(1 / 0)
-    return "500 Internal Server Error", "text/plain", payload
-
-
-def handle_404(method: str, path: str, qs: str) -> ResponseT:
-    status = "404 Not Found"
-    content_type = "text/plain"
-    payload = f"OOPS! endpoint {path} not found!"
-    return status, content_type, payload
-
-
-def handle_task_303(method: str, path: str, qs: str) -> ResponseT:
-    status = "200 OK"
-    content_type = "text/html"
-
-    qsi = parse_qs(qs)
-
-    template = read_template("task_303.html")
-
-    sentence = qsi.get("sentence")
-
-    if not sentence:
-        result = ""
-    else:
-        sentence = sentence[0]
-        result = task303.solution(sentence)
-
-    payload = template.format(text=result)
-
-    return status, content_type, payload
-
-
-def handle_index(method: str, path: str, qs: str) -> ResponseT:
-    status = "200 OK"
-
-    content_type = "text/html"
-
-    template = read_template("index.html")
-
-    payload = template.format(
-        random_number=123,
-        environ={},
-    )
-
-    return status, content_type, payload
-
-
-HANDLERS = {
-    "/": handle_index,
-    "/e/": handle_error,
-    "/tasks/lesson03/task303/": handle_task_303,
-}
-
-
-def get_handler(path: str) -> Callable:
-    handler = HANDLERS.get(path, handle_404)
-
-    return handler
-
 
 def application(environ, start_response):
-    method = environ["REQUEST_METHOD"]
-    path = environ["PATH_INFO"]
-    query_string = environ["QUERY_STRING"]
+    request = build_request(environ)
+    handler = get_handler(request)
 
-    handler = get_handler(path)
-
-    status, content_type, payload = handler(method, path, query_string)
+    try:
+        response = handler(request)
+    except Exception:
+        response = handle_500(request)
 
     headers = {
-        "Content-type": content_type,
+        "Content-Type": response.content_type,
     }
 
-    start_response(status, list(headers.items()))
+    start_response(
+        f"{response.status.value} {response.status.phrase}",
+        list(headers.items()),
+    )
 
-    yield payload.encode()
-
-
-def read_template(template_name: str) -> str:
-    dir_templates = DIR_SRC / "main" / "templates"
-    template = dir_templates / template_name
-
-    assert template.is_file()
-
-    with template.open("r") as fd:
-        content = fd.read()
-
-    return content
+    yield response.payload.encode()
