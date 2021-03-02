375ea4e7ca9e7e56a0b0a2b1fcfa43e6acc97f3c
Alexander Sidorov <alexander@sidorov.dev>
2021-03-02 12:26:44 +0300

    included task402 app in project routing

--- src/project/urls.py
+++ src/project/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.urls import include
 from django.urls import path
 
 from project import views
@@ -6,4 +7,5 @@ from project import views
 urlpatterns = [
     path("", views.index),
     path("admin/", admin.site.urls),
+    path("tasks/402/", include("applications.task402.urls")),
 ]
