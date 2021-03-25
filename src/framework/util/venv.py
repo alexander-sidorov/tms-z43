0a56edcd721efb7f1a673a5975baf8fcd99fd4ff
Alexander Sidorov <76160017+alexander-sidorov@users.noreply.github.com>
2021-03-25 21:32:01 +0300

    Heroku fix (#12)
    * xxx
    
    * python runtime upgraded from 3.9.1 to 3.9.2
    
    * formatting
    
    Co-authored-by: Alexander Sidorov <alexander@sidorov.dev>

--- src/framework/util/venv.py
+++ src/framework/util/venv.py
@@ -11,9 +11,9 @@ import sys
 
 def in_virtualenv():
     try:
-        from framework.config import settings
+        from framework.util.settings import get_setting
 
-        synth_venv = settings.VENV_SYNTHETIC
+        synth_venv = get_setting("VENV_SYNTHETIC")
     except ImportError:
         env_value = os.getenv("VENV_SYNTHETIC", "False").capitalize()
         synth_venv = bool(eval(env_value))
